20ef96f469823cbdf8aecfb0d3581376b7545b55
Hyunil Yoo <hyunilg@gmail.com>
2021-05-05 18:54:54 -0700

    refactored tables spider

--- tables_crawler/tables_crawler/spiders/tables_spider.py
+++ tables_crawler/tables_crawler/spiders/tables_spider.py
@@ -12,72 +12,48 @@ class TableSpider(scrapy.Spider):
     
     def __init__(self):
         scrapy.Spider.__init__(self)
-        self.browser = webdriver.Chrome("C:\sta\chromedriver.exe")
-        self.browser.set_window_size(1360, 768)
+        self.browser = webdriver.Chrome('/Users/hyunilyoo/Documents/analytics/chromedriver')
         
     def parse(self, response):
         self.browser.get(response.url)
+        time.sleep(5)
+        self.html = self.browser.find_element_by_xpath('//*').get_attribute('outerHTML')
+        self.selector = Selector(text=self.html)
+        self.season_epl = len(self.selector.xpath('//*[@id="mainContent"]/div[2]/div[1]/div[1]/section/div[2]/ul/li'))+1
         
-        for i in range(2,30):
-            time.sleep(8)
-            html = self.browser.find_element_by_xpath('//*').get_attribute('outerHTML')
-            selector = Selector(text=html)
+        # Season dropdown
+        for i in range(2, self.season_epl):
+            time.sleep(5)
             self.browser.find_element_by_xpath('//*[@id="mainContent"]/div[2]/div[1]/div[1]/section/div[2]/div[2]').click()
             time.sleep(3)
-            if i < 10: 
-                self.browser.find_element_by_xpath('//*[@id="mainContent"]/div[2]/div[1]/div[1]/section/div[2]/ul/li'+str([i])).click()
-                time.sleep(8)
 
-                html = self.browser.find_element_by_xpath('//*').get_attribute('outerHTML')
-                selector = Selector(text=html)
-                rows = selector.xpath('//*[@id="mainContent"]/div[2]/div[1]/div[4]/div/div/div/table/tbody/tr[not(@class="expandable")]')
-                for row in rows:
-                    item = TablesCrawlerItem()
-                    item["club_name"] = row.xpath('./td[3]/a/span[2]/text()')[0].extract()
-                    item["position"] = row.xpath('./td[2]/span[1]/text()')[0].extract()
-                    item["won"] = row.xpath('./td[5]/text()')[0].extract()
-                    item["lost"] = row.xpath('./td[7]/text()')[0].extract()
-                    item["drawn"] = row.xpath('./td[6]/text()')[0].extract()
-                    item["goal"] = row.xpath('./td[8]/text()')[0].extract()
-                    item["goal_against"] = row.xpath('./td[9]/text()')[0].extract()
-                    item["points"] = row.xpath('./td[11]/text()')[0].extract()
-                    yield item
+            if i < 10: 
+                self.browser.find_element_by_xpath('//*[@id="mainContent"]/div[2]/div[1]/div[1]/section/div[2]/ul/li'+f'[{i}]').click()
+
             elif i >= 10 and i < 19:
                 self.browser.execute_script('window.scrollTo(0, 150)')
-                self.browser.find_element_by_xpath('//*[@id="mainContent"]/div[2]/div[1]/div[1]/section/div[2]/ul/li'+str([i])).click()
-                time.sleep(8)
+                self.browser.find_element_by_xpath('//*[@id="mainContent"]/div[2]/div[1]/div[1]/section/div[2]/ul/li'+f'[{i}]').click()
 
-                html = self.browser.find_element_by_xpath('//*').get_attribute('outerHTML')
-                selector = Selector(text=html)
-                rows = selector.xpath('//*[@id="mainContent"]/div[2]/div[1]/div[4]/div/div/div/table/tbody/tr[not(@class="expandable")]')
-                for row in rows:
-                    item = TablesCrawlerItem()
-                    item["club_name"] = row.xpath('./td[3]/a/span[2]/text()')[0].extract()
-                    item["position"] = row.xpath('./td[2]/span[1]/text()')[0].extract()
-                    item["won"] = row.xpath('./td[5]/text()')[0].extract()
-                    item["lost"] = row.xpath('./td[7]/text()')[0].extract()
-                    item["drawn"] = row.xpath('./td[6]/text()')[0].extract()
-                    item["goal"] = row.xpath('./td[8]/text()')[0].extract()
-                    item["goal_against"] = row.xpath('./td[9]/text()')[0].extract()
-                    item["points"] = row.xpath('./td[11]/text()')[0].extract()
-                    yield item
-            elif i >= 19:
+            else:
                 self.browser.execute_script('window.scrollTo(0, 330)')
-                self.browser.find_element_by_xpath('//*[@id="mainContent"]/div[2]/div[1]/div[1]/section/div[2]/ul/li'+str([i])).click()
-                time.sleep(8)
+                self.browser.find_element_by_xpath('//*[@id="mainContent"]/div[2]/div[1]/div[1]/section/div[2]/ul/li'+f'[{i}]').click()
 
-                html = self.browser.find_element_by_xpath('//*').get_attribute('outerHTML')
-                selector = Selector(text=html)
-                rows = selector.xpath('//*[@id="mainContent"]/div[2]/div[1]/div[4]/div/div/div/table/tbody/tr[not(@class="expandable")]')
-                for row in rows:
-                    item = TablesCrawlerItem()
-                    item["club_name"] = row.xpath('./td[3]/a/span[2]/text()')[0].extract()
-                    item["position"] = row.xpath('./td[2]/span[1]/text()')[0].extract()
-                    item["won"] = row.xpath('./td[5]/text()')[0].extract()
-                    item["lost"] = row.xpath('./td[7]/text()')[0].extract()
-                    item["drawn"] = row.xpath('./td[6]/text()')[0].extract()
-                    item["goal"] = row.xpath('./td[8]/text()')[0].extract()
-                    item["goal_against"] = row.xpath('./td[9]/text()')[0].extract()
-                    item["points"] = row.xpath('./td[11]/text()')[0].extract()
-                    yield item
-                    
+            time.sleep(5)
+            self.html = self.browser.find_element_by_xpath('//*').get_attribute('outerHTML')
+            self.selector = Selector(text=self.html)
+            self.rows = self.selector.xpath('//*[@id="mainContent"]/div[2]/div[1]/div[5]/div/div/div/table/tbody/tr[not(@class="expandable")]')
+
+            for row in self.rows:
+                self.item = TablesCrawlerItem()
+                self.item["club_name"] = row.xpath('./td[3]/a/span[2]/text()')[0].extract()
+                self.item["position"] = row.xpath('./td[2]/span[1]/text()')[0].extract()
+                self.item["won"] = row.xpath('./td[5]/text()')[0].extract()
+                self.item["lost"] = row.xpath('./td[7]/text()')[0].extract()
+                self.item["drawn"] = row.xpath('./td[6]/text()')[0].extract()
+                self.item["goal"] = row.xpath('./td[8]/text()')[0].extract()
+                self.item["goal_against"] = row.xpath('./td[9]/text()')[0].extract()
+                self.item["points"] = row.xpath('./td[11]/text()')[0].extract()
+                yield self.item
+            
+        self.browser.quit()
+
